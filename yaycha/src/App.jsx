e9648b7ad6c42b58401479461264f41638555cc0
unknown <yezawhtet.web@gmail.com>
2024-09-05 15:37:55 +0630

    initial commit

---
+++ yaycha/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import List from "./List";
+import Item from "./Item";
+import Form from "./Form";
+
+export default function App() {
+  const [data, setData] = useState([
+    { id: 1, content: "Hello, World!", name: "Alice" },
+    { id: 2, content: "React is fun.", name: "Bob" },
+    { id: 3, content: "Yay, interesting.", name: "Chris" },
+  ]);
+
+  const remove = id => {
+    setData(data.filter(item => item.id !== id));
+  };
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, content, name }]);
+    };
+
+  return (
+    <div style={{ maxWidth: 600, margin: "20px auto" }}>
+      <h1>YayCha</h1>
+      <Form add={add} />
+
+      <List>
+        {data.map(item => (
+           <Item key={item.id} item={item} remove={remove} />
+        ))}
+      </List>
+    </div>
+  )
+}
