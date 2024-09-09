fe539af27853764217bf371f37076c7d909f7942
unknown <yezawhtet.web@gmail.com>
2024-09-09 08:59:49 +0630

    adding mui releted files and notes

--- yaycha/src/App.jsx
+++ yaycha/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState, useContext } from "react";
-import { AppContext } from "./ThemedApp";
-import List from "./List";
-import Item from "./Item";
-import Form from "./Form";
+import { useState } from "react";
+import { Box, Container } from "@mui/material";
+import Header from "./components/Header";
+import Item from "./components/Item";
+import Form from "./components/Form";
+import { useApp } from "./ThemedApp";
 
 export default function App() {
-  const [showForm, setShowForm] = useState(false);
-  const { mode, setMode } = useContext(AppContext);
+  const {showForm} = useApp();
 
   const [data, setData] = useState([
-    { id: 1, content: "Hello, World!", name: "Alice" },
-    { id: 2, content: "React is fun.", name: "Bob" },
     { id: 3, content: "Yay, interesting.", name: "Chris" },
+    { id: 2, content: "React is fun.", name: "Bob" },
+    { id: 1, content: "Hello, World!", name: "Alice" },
   ]);
 
   const remove = id => {
@@ -19,61 +19,31 @@ export default function App() {
   };
 
   const add = (content, name) => {
-    const id = data[data.length - 1].id + 1;
-    setData([...data, { id, content, name }]);
+    const id = data[0].id + 1;
+    setData([{ id, content, name }, ...data]);
   };
+  // const add = (content, name) => {
+  //   const id = data[data.length - 1].id + 1;
+  //   setData([...data, { id, content, name }]);
+  // };
 
   return (
-    <div
-      style={{
-        minHeight: 1500,
-        background: mode === "dark" ? "black" : "white",
-        color: mode === "dark" ? "white" : "black",
-        paddingTop: 20,
-      }}>
-      <div style={{ maxWidth: 600, margin: "20px auto" }}>
-        <h1
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >YayCha<button
-          onClick={() => setShowForm(!showForm)}
-          style={{
-            width: 32,
-            height: 32,
-            borderRadius: 50,
-            fontWeight: "bold",
-            border: "0 none",
-            background: showForm ? "#dc3545" : "#0d6efd",
-            color: "white",
-          }}>
-            {showForm ? "×" : "+"}
-          </button>
-          <button
-          onClick={() =>
-            setMode(mode === "dark" ? "light" : "dark")
-          }
-          style={{
-            marginLeft: 8,
-            padding: "0 20px",
-            height: 32,
-            borderRadius: 32,
-            border: "0 none",
-            background: mode === "dark" ? "#333" : "#ddd",
-            color: mode === "dark" ? "white" : "black",
-          }}>
-          {mode === "dark" ? "Light" : "Dark"}</button>
-        </h1>
+    <Box>
+      <Header />
+      <Container
+        maxWidth="sm"
+        sx={{ mt: 4 }}>
         {showForm && <Form add={add} />}
-
-        <List>
-          {data.map(item => (
-            <Item key={item.id} item={item} remove={remove} />
-          ))}
-        </List>
-      </div>
-    </div>
+        {data.map(item => {
+          return (
+            <Item
+              key={item.id}
+              item={item}
+              remove={remove}
+            />
+          );
+        })}
+      </Container>
+    </Box>
   )
 }
--- yaycha/src/ThemedApp.jsx
+++ yaycha/src/ThemedApp.jsx
@@ -1,15 +1,29 @@
-import { createContext, useState } from "react";
-
+import { createContext, useContext, useState } from "react";
 import App from "./App";
+//import for MUI related
+import {CssBaseline, ThemeProvider, createTheme} from "@mui/material";
+
+//MUI
+const theme= createTheme({
+    palette:{
+        mode:"dark",
+    },
+});
 
 export const AppContext = createContext();
 
+export function useApp(){
+    return useContext(AppContext);
+}
+
 export default function ThemedApp(){
-    const [mode, setMode] = useState("dark");
-    
+    const [showForm, setShowForm] = useState(false);
     return (
-        <AppContext.Provider value={{mode, setMode}}>
+        <ThemeProvider theme={theme}>
+        <AppContext.Provider value={{showForm, setShowForm}}>
             <App/>
+            <CssBaseline/>  
         </AppContext.Provider>
+        </ThemeProvider>
     )
 }
